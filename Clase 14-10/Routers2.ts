6b03546597e99bd6cfa99a7d91680e00d0fba89f
David <43136464+dmartinezc4@users.noreply.github.com>
2022-10-14 13:38:17 +0200

    Create Routers2.ts
    No funciona en el localhost

---
+++ Clase 14-10/Routers2.ts
@@ -0,0 +1,61 @@
+import { Application,Router } from "https://deno.land/x/oak@v11.1.0/mod.ts"
+
+
+const app = new Application();
+
+const router = new Router();
+
+
+const getCharacters =async(page:number):Promise<any>=>{
+  const data = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`,
+  );
+  return await data.json()
+}
+
+
+router.get("/hora",(ctx)=>{
+  ctx.response.body = new Date().getTime().toString();
+})
+.get("/characters/page",async (ctx)=>{//Es asíncrona porque esto es pedirle datos a la api de rick y morty
+  let page = Number(ctx.params?.page);
+
+  if(page>42){
+    ctx.response.body ="Invalid Page Number";
+    ctx.response.status=500;
+    return;
+  }if(page<1){
+    const jsonresponse=await getCharacters(1);
+    ctx.response.body=jsonresponse;
+    ctx.response.status=200;
+    ctx.response.type= "application/json";
+  }else{
+    const jsonresponse=await getCharacters(page);
+    ctx.response.body=jsonresponse;
+    ctx.response.status=200;
+    ctx.response.type= "application/json";
+  }
+  
+})
+.get("/characters",async (ctx)=>{//Es asíncrona porque esto es pedirle datos a la api de rick y morty
+  
+  const jsonresponse=await fetch("https://rickandmortyapi.com/api/character/?page=1");
+  const jsonData=await jsonresponse.json();
+  ctx.response.body=jsonData;
+  ctx.response.type= "application/json";
+})
+.get("/dia/:id",(ctx)=>{
+  if(Number(ctx?.params?.id)>7){
+    ctx.response.body ="Invalid Day";
+    ctx.response.status=500;
+  }else{
+    ctx.response.body= {
+      dia:new Date().getDay().toString(),
+      n:Number(ctx?.params.id),
+  };
+  }
+});
+
+app.use(router.routes())
+
+await app.listen({ port: 8000 });
+
